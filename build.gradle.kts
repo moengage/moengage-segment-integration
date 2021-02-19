d696a41a6689af2067aa98bd17bd309a45a83bd5
Umang Chamaria <umang@moengage.com>
2021-02-19 15:10:29 +0530

    Maven Central Setup
    * initial commit
    
    * POM meta update
    
    * adding missed pom url

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+buildscript {
+  repositories {
+    google()
+    jcenter()
+  }
+  dependencies {
+    classpath("com.android.tools.build:gradle:4.1.1")
+    classpath("com.google.gms:google-services:4.3.4")
+    classpath("com.vanniktech:gradle-maven-publish-plugin:0.13.0")
+  }
+}
+
+allprojects {
+  repositories {
+    google()
+    jcenter()
+  }
+}
+
+tasks.register<Delete>("clean") {
+  delete(rootProject.buildDir)
+}
