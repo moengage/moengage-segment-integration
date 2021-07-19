5fbac6ba6469781699ce8a68a9b16b319ca66a50
umangmoe <umang@moengage.com>
2021-07-19 11:21:29 +0530

    MoEngage SDK version updated to 11.3.01

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -2,6 +2,6 @@
  * @author Umang Chamaria
  */
 object Deps {
-    const val moengage = "com.moengage:moe-android-sdk:11.3.00"
+    const val moengage = "com.moengage:moe-android-sdk:11.3.01"
     const val segment = "com.segment.analytics.android:analytics:4.8.2"
 }
