8161da8a3e704be2bb2701f42c1a4347c4a7cec2
umangmoe <umang@moengage.com>
2022-04-22 08:19:25 +0530

    initial commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,26 @@
-enableFeaturePreview("VERSION_CATALOGS")
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven(url = "https://developer.huawei.com/repo/")
+    }
+}
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        google()
         mavenCentral()
+        maven(url = "https://developer.huawei.com/repo/")
     }
     versionCatalogs {
+        create("moengageInternal") {
+            from("com.moengage:android-dependency-catalog-internal:1.1.0")
+        }
         create("moengage") {
             from("com.moengage:android-dependency-catalog:2.1.0")
-        }
-        create("moengageInternal") {
-            from("com.moengage:android-dependency-catalog-internal:1.0.0")
         }
     }
 }
