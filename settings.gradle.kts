98a27597645efaf0ae59faeb0cbd401c48073849
umangmoe <umang@moengage.com>
2022-10-17 18:05:44 +0530

    initial commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,10 +14,11 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
     }
     versionCatalogs {
         create("moengageInternal") {
-            from("com.moengage:android-dependency-catalog-internal:1.1.0")
+            from("com.moengage:android-dependency-catalog-internal:1.2.1-SNAPSHOT")
         }
         create("moengage") {
             from("com.moengage:android-dependency-catalog:2.5.1")
