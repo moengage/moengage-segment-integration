8cdf3e0322380c348cc82f5fd3f8e7394c86700d
abhishek-engg-mobilesdk <abhishek.kumar@moengage.com>
2025-01-09 16:44:17 +0530

    MOEN-36211: AGP Version update from `8.4.0` to `8.7.3`

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,13 +12,14 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
     }
     versionCatalogs {
         create("moengageInternal") {
             from("com.moengage:android-dependency-catalog-internal:2.2.0")
         }
         create("moengage") {
-            from("com.moengage:android-dependency-catalog:4.5.3")
+            from("com.moengage:android-dependency-catalog:4.6.0")
         }
     }
 }
