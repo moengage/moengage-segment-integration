a90010328b14837b067e093b818c04207012a9bd
umangmoe <umang@moengage.com>
2025-07-25 16:33:46 +0530

    MoEngage handler update initial commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URI
+
 pluginManagement {
     repositories {
         google()
@@ -11,6 +13,16 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
+        maven {
+            name = "Central Portal Snapshots"
+            url = URI("https://central.sonatype.com/repository/maven-snapshots/")
+
+            // Only search this repository for the specific dependency
+            content {
+                includeModule("com.moengage", "moe-android-sdk")
+                includeModule("com.moengage", "core")
+            }
+        }
         mavenCentral()
     }
     versionCatalogs {
